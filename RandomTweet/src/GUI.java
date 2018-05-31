62482e21c4aaa78a3ee04098cd8ee8db9679d0de
dmitryblackwell <dmitryblackwell@gmail.com>
2018-06-01 00:25:52 +0300

    Random tweet from my feed.
    Ok, for now my app, show random tweet from my home line.

--- RandomTweet/src/GUI.java
+++ RandomTweet/src/GUI.java
@@ -1,21 +1,31 @@
 import javafx.application.Application;
-import javafx.event.ActionEvent;
-import javafx.event.EventHandler;
+import javafx.geometry.Insets;
 import javafx.geometry.Pos;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
 import javafx.scene.control.TextField;
 import javafx.scene.image.Image;
+import javafx.scene.input.DataFormat;
 import javafx.scene.layout.HBox;
 import javafx.scene.layout.StackPane;
 import javafx.scene.layout.VBox;
 import javafx.scene.paint.ImagePattern;
 import javafx.scene.shape.Circle;
 import javafx.scene.text.Font;
-import javafx.scene.text.Text;
 import javafx.stage.Stage;
 import javafx.stage.StageStyle;
+import twitter4j.Status;
+import twitter4j.Twitter;
+import twitter4j.TwitterException;
+import twitter4j.TwitterFactory;
+import twitter4j.api.TweetsResources;
+import twitter4j.conf.ConfigurationBuilder;
+
+import java.text.DateFormat;
+import java.text.SimpleDateFormat;
+import java.util.List;
+import java.util.Random;
 
 
 public class GUI extends Application {
@@ -23,18 +33,44 @@ public class GUI extends Application {
     public static void main(String[] args) { launch(args); }
 
     private Circle circle = new Circle(100,100,50);
-    private Label tweetText = new Label();
+    private Label tweetLabel = new Label();
+    private Label nameLabel = new Label();
+    private Label timeLabel = new Label();
 
-    private void dataUpdate(String imgRoot, String name, String tweet){
+    private void dataUpdate(String imgRoot, String name, String time, String tweet){
         Image img = new Image(imgRoot);
         circle.setFill(new ImagePattern(img));
-        tweetText.setText(name + "\n" + tweet);
+        nameLabel.setText(name);
+        timeLabel.setText(time);
+        tweetLabel.setText(tweet);
     }
 
-    private void tweetUpdate(String login){
-        dataUpdate("https://pbs.twimg.com/profile_images/661330385465245696/3rnsJokZ_400x400.jpg", "Alex Hirsch",
-                "Some say that having my signature in your home or barn will ward off witches/ keep your livestock from giving poisoned milk, don’t you know.");
 
+    DateFormat dateFormat = new SimpleDateFormat("MM/dd/yyyy HH:mm");
+    private void tweetUpdate(String login) {
+
+        ConfigurationBuilder builder = new ConfigurationBuilder();
+        // This keys is read only... sorry )))
+        builder.setDebugEnabled(true)
+                .setOAuthConsumerKey("o5Y3KTO80LuooTMrqioklMQYy")
+                .setOAuthConsumerSecret("7cpWmhvu6iyPvvxJ4RHAfQdVlCzf73ASHZb6cH2lQisOw07yjU")
+                .setOAuthAccessToken("4482344913-O2xj79vEictmpxIew4ldzQC07BOSOpKPCwTUqrO")
+                .setOAuthAccessTokenSecret("i3XGeHnLZHbjg169m7eviAs0NtCRPB1GHKL5I78gfIudf");
+
+        TwitterFactory factory = new TwitterFactory(builder.build());
+        Twitter twitter = factory.getInstance();
+
+        try {
+            List<Status> statuses = twitter.getHomeTimeline();
+            Random R = new Random();
+            Status status = statuses.get(R.nextInt(statuses.size()));
+            dataUpdate(status.getUser().getBiggerProfileImageURL(),
+                    status.getUser().getScreenName(),
+                    dateFormat.format(status.getCreatedAt()),
+                    status.getText());
+        } catch (TwitterException e) {
+            e.printStackTrace();
+        }
     }
 
     @Override
@@ -46,12 +82,24 @@ public class GUI extends Application {
         vBox.setAlignment(Pos.CENTER);
 
         HBox tweetBox = new HBox();
+        tweetBox.setSpacing(30);
+        tweetBox.setPadding(new Insets(20));
         tweetBox.setAlignment(Pos.CENTER);
-        tweetText.setWrapText(true);
-        tweetBox.getChildren().addAll(circle, tweetText);
+
+        VBox tweetTextBox = new VBox();
+        tweetLabel.setWrapText(true);
+        tweetLabel.setFont(new Font(15));
+
+        nameLabel.setFont(new Font(30));
+        nameLabel.setStyle("-fx-font-weight: bold");
+        timeLabel.setFont(new Font(10));
+
+        tweetTextBox.getChildren().addAll(nameLabel, timeLabel,tweetLabel);
+        tweetBox.getChildren().addAll(circle, tweetTextBox);
 
         HBox inputBox = new HBox();
-        inputBox.setAlignment(Pos.CENTER);
+        inputBox.setSpacing(10);
+        inputBox.setAlignment(Pos.BOTTOM_CENTER);
 
         Label loginLabel = new Label("@");
         loginLabel.setFont(new Font(30));
@@ -65,7 +113,7 @@ public class GUI extends Application {
         StackPane root = new StackPane();
         root.getChildren().addAll(vBox);
         primaryStage.setTitle("RandomTweet");
-        Scene scene = new Scene(root, 400, 700);
+        Scene scene = new Scene(root, 600, 400);
         primaryStage.setScene(scene);
         primaryStage.setResizable(false);
         primaryStage.show();
