6a593546cf6e968adc2de715b0a1c7df14c589b9
dmitryblackwell <dmitryblackwell@gmail.com>
2018-06-01 15:55:45 +0300

    Tokens Pattern
    Create pattern for token keys, so you just parse your tokens and can run project without creating new empty classes.

---
+++ RandomTweet/src/com/blackwell/TwitterKeys.java
@@ -0,0 +1,11 @@
+package com.blackwell;
+
+class TwitterKeys {
+    static final String ConsumerKey = "";
+
+    static final String ConsumerSecret = "";
+
+    static final String AccessToken = "";
+
+    static final String AccessTokenSecret = "";
+}
---
+++ RandomTweet/src/org/george/TwitterKeys.java
@@ -0,0 +1,11 @@
+package org.george;
+
+class TwitterKeys {
+    static final String ConsumerKey = "";
+
+    static final String ConsumerSecret = "";
+
+    static final String AccessToken = "";
+
+    static final String AccessTokenSecret = "";
+}
